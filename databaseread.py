e01cf33c241b80bd3c7d4e4d90ad4efc4636ec20
efe.demir <efe.demir@eliarge.com>
2023-07-28 13:29:34 +0300

    ilk calisan hali

---
+++ databaseread.py
@@ -0,0 +1,41 @@
+import psycopg2
+from psycopg2 import Error
+from dotenv import dotenv_values
+import pandas as pd
+
+env_vars = dotenv_values()
+
+def dbread():
+
+    try:
+        connection = psycopg2.connect(
+            user=env_vars.get('PG_DB_USERNAME'),
+            password=env_vars.get('PG_DB_PASSWORD'),
+            host=env_vars.get('PG_DB_SERVER'),
+            port=env_vars.get('PG_DB_PORT'),
+            database=env_vars.get('PG_DB_DATABASE')
+        )
+        cursor = connection.cursor()
+        
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL:", error)
+    
+    try:
+    
+        cursor.execute("""SELECT yabby_kod, TO_CHAR(datalogged, 'YYYY-MM-DD') as datalogged, 
+                          battery FROM mind4machine.textracklocationcheck
+                          group by TO_CHAR(datalogged, 'YYYY-MM-DD'),yabby_kod,battery order by datalogged asc;""")
+        temp = cursor.fetchall()
+        df = pd.DataFrame(temp)
+        mapping = {df.columns[0]:'yabby_kod', df.columns[1]:'datalogged', df.columns[2]:'battery'}
+        df = df.rename(columns=mapping)
+        return df
+    
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
---
+++ main.py
@@ -0,0 +1,11 @@
+import databaseread as  dbc
+import battery_pred as  batprd
+
+def main():
+    data = dbc.dbread()
+    batprd.battery_predict(data)
+    
+
+if __name__ == "__main__":
+    main()
+ 
